f1c8debbff6041a336bdfdbd1eb3fe0dbb56951d
Sylvia Schmidt <sylvia.schmidt@jielo.de>
2015-10-03 15:26:00 +0200

    Improved Javascripts and Webfonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,37 @@
-var gulp = require('gulp'); 
+var gulp = require('gulp');
+var deploy = require('gulp-gh-pages');
+var scripts = require('gulp-uglify');
+var styles = require('gulp-minify-css');
+var inline = require('gulp-inline-source');
+
+gulp.task('scripts', function(){
+	gulp.src('dev/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js/'));
+	
+	gulp.src('dev/views/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/views/js/'));
+});
+
+/*
+gulp.task('styles', function(){
+	gulp.src('dev/css/*.css')
+		.pipe(minify())
+		.pipe(gulp.dest('dist/css/'));
+	
+	gulp.src('dev/views/css/*.css')
+		.pipe(minify())
+		.pipe(gulp.dest('dist/views/css/'));
+});
+*/
+
+
+
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
 
 gulp.task('default', function() {
 	console.log('Gulp is running');
