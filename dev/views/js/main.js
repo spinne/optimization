f193610d0d7bd97e09d8436f1e6f97aa6d31b700
Sylvia Schmidt <sylvia.schmidt@jielo.de>
2015-10-03 18:24:18 +0200

    Commit for savekeeping

--- dev/views/js/main.js
+++ dev/views/js/main.js
@@ -523,17 +523,27 @@ window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
-  var s = 256;
-  for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
-    elem.basicLeft = (i % cols) * s;
-    elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
-  }
-  updatePositions();
+	// Calculating instead of setting number of pizzas needed to cover the visible screen
+	// By dividing the screen into rows and collums of 256px height / width
+	// Rounding up (Math.ceil) to make sure patially visible pizza rows or collums are included.
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	var cols = Math.ceil(w / 256);
+	var rows = Math.ceil(h / 256);
+	var s = 256;
+	var numberPizzas = cols * rows;
+	var parent = document.getElementById('movingPizzas1');
+	
+	for (var i = 0; i < numberPizzas; i++) {
+		var elem = document.createElement('img');
+		elem.className = 'mover';
+		elem.src = "images/pizza.png";
+		elem.style.height = "100px";
+		elem.style.width = "73.333px";
+		elem.style.willChange = 'left';
+		elem.basicLeft = (i % cols) * s;
+		elem.style.top = (Math.floor(i / cols) * s) + 'px';
+		parent.appendChild(elem);
+	}
+	updatePositions();
 });
